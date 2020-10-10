d22675a3b2195943153fd15cf2fac1cae22387d1
Daniel Jonsson <daniel@jonsson.xyz>
2020-10-10 11:42:58 +0200

    Implement parts to make menu item tests pass

--- server.js
+++ server.js
@@ -222,3 +222,208 @@ app.delete('/api/employees/:employeeId/timesheets/:timesheetId', (req, res, next
   )
 })
 
+app.get('/api/menus', (req, res, next) => {
+  db.all('SELECT * FROM Menu', (err, rows) => {
+    res.json({menus: rows})
+  })
+})
+
+app.post('/api/menus', (req, res, next) => {
+  let { title } = req.body.menu
+  if (!title) {
+    return res.sendStatus(400)
+  }
+
+  db.run(
+    `INSERT INTO Menu (title)
+     VALUES ($title)`,
+    {
+      $title: title
+    },
+    function(err) {
+      if (err) {
+        next(err)
+      } else {
+        db.get(
+          'SELECT * FROM Menu WHERE id=$id',
+          {$id: this.lastID},
+          (error, menu) => {
+            res.status(201).json({menu: menu})
+          }
+        )
+      }
+    }
+  )
+})
+
+app.param('menuId', (req, res, next, menuId) => {
+  db.get('SELECT * FROM Menu WHERE id = $menuId', {$menuId: menuId}, (err, row) => {
+    if (err) {
+      next(err)
+    } else if (row) {
+      req.menu = row
+      next()
+    } else {
+      res.status(404).send()
+    }
+  })
+})
+
+app.get('/api/menus/:menuId', (req, res, next) => {
+  res.json({menu: req.menu})
+})
+
+app.put('/api/menus/:menuId', (req, res, next) => {
+  let { title } = req.body.menu
+  if (!title) {
+    return res.sendStatus(400)
+  }
+
+  db.run(
+    `UPDATE Menu
+     SET title=$title
+     WHERE id=$id`,
+    {
+      $title: title,
+      $id: req.menu.id
+    },
+    err => {
+      if (err) {
+        next(err)
+      } else {
+        db.get(
+          'SELECT * FROM Menu WHERE id=$id',
+          {$id: req.menu.id},
+          (error, menu) => {
+            res.status(200).json({menu: menu})
+          }
+        )
+      }
+    }
+  )
+})
+
+app.delete('/api/menus/:menuId', (req, res, next) => {
+  db.get('SELECT * FROM MenuItem WHERE menu_id = $menuId', {$menuId: req.menu.id}, (err, row) => {
+    if (err) {
+      next(err)
+    } else if (row) {
+      res.sendStatus(400)
+      next()
+    } else {
+      db.run(
+        `DELETE FROM Menu WHERE id=$id`,
+        {
+          $id: req.menu.id
+        },
+        function(err) {
+          if (err) {
+            next(err)
+          } else {
+            res.sendStatus(204)
+          }
+        }
+      )
+    }
+  })
+})
+
+app.get('/api/menus/:menuId/menu-items', (req, res, next) => {
+  db.all('SELECT * FROM MenuItem Where menu_id=$menuId', {$menuId: req.menu.id}, (err, rows) => {
+    res.json({menuItems: rows})
+  })
+})
+
+app.post('/api/menus/:menuId/menu-items', (req, res, next) => {
+  let { name, description, inventory, price } = req.body.menuItem
+  if (!name || !description || !inventory || !price) {
+    return res.sendStatus(400)
+  }
+
+  db.run(
+    `INSERT INTO MenuItem (name, description, inventory, price, menu_id)
+     VALUES ($name, $description, $inventory, $price, $menuId)`,
+    {
+      $name: name,
+      $description: description,
+      $inventory: inventory,
+      $price: price,
+      $menuId: req.menu.id
+    },
+    function(err) {
+      if (err) {
+        next(err)
+      } else {
+        db.get(
+          'SELECT * FROM MenuItem WHERE id=$id',
+          {$id: this.lastID},
+          (error, menuItem) => {
+            res.status(201).json({menuItem: menuItem})
+          }
+        )
+      }
+    }
+  )
+})
+
+app.param('menuItemId', (req, res, next, menuItemId) => {
+  db.get('SELECT * FROM MenuItem WHERE id = $menuItemId', {$menuItemId: menuItemId}, (err, row) => {
+    if (err) {
+      next(err)
+    } else if (row) {
+      req.menuItem = row
+      next()
+    } else {
+      res.status(404).send()
+    }
+  })
+})
+
+app.put('/api/menus/:menuId/menu-items/:menuItemId', (req, res, next) => {
+  let { name, description, inventory, price } = req.body.menuItem
+  if (!name || !description || !inventory || !price) {
+    return res.sendStatus(400)
+  }
+
+  db.run(
+    `UPDATE MenuItem
+     SET name=$name, description=$description, inventory=$inventory, price=$price
+     WHERE id=$id`,
+    {
+      $name: name,
+      $description: description,
+      $inventory: inventory,
+      $price: price,
+      $id: req.menuItem.id
+    },
+    err => {
+      if (err) {
+        next(err)
+      } else {
+        db.get(
+          'SELECT * FROM MenuItem WHERE id=$id',
+          {$id: req.menuItem.id},
+          (error, menuItem) => {
+            res.status(200).json({menuItem: menuItem})
+          }
+        )
+      }
+    }
+  )
+})
+
+app.delete('/api/menus/:menuId/menu-items/:menuItemId', (req, res, next) => {
+  db.run(
+    `DELETE FROM MenuItem WHERE id=$id`,
+    {
+      $id: req.menuItem.id
+    },
+    function(err) {
+      if (err) {
+        next(err)
+      } else {
+        res.sendStatus(204)
+      }
+    }
+  )
+})
